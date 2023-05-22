e9acde7d520d4e9f153c226aaf455087a793b783
Wolfmyths <118236986+Wolfmyths@users.noreply.github.com>
2023-05-21 23:51:00 -0400

    Replaced env vars with config parser (issue #1) (#6)
    * Updated mention command
    
    Changed condition on line 86 to check if the message was saying hello to the bot.
    
    In the elif statement starting line 86 created a variable with a ternary operator to use the message author's nickname if they have one
    
    * Update from prev commit.py
    
    Replaced the sender variable with message.author.display_name, as this does the same thing
    
    * Create config.ini
    
    Created config.ini to replace environment variables
    
    * Update config.ini
    
    added custom_prefix setting
    
    * Replaced env vars with config parser
    
    Replaced os import with configparser (os isn't being used atm)
    
    Removed environment variables and replaced them with ConfigParser.
    
    Added the COMMAND_CHAR variable as a configurable setting.
    
    Added type hinting for the member parameter in the function on_member_join for clarity.
    
    * Updated handle_command
    
    Adapted match case statement to work with the new command prefix config variable

--- botScript.py
+++ botScript.py
@@ -1,25 +1,33 @@
-import os
+from configparser import ConfigParser
 import discord
 
 # ----- Load and check variables ----- #
-# Load environment variables
-TOKEN = os.getenv('DISCORD_TOKEN')
-GUILD_ID = os.getenv('DISCORD_GUILD_ID')
 
-# If either environment variable isn't present, print an error and quit
+# Initialize ConfigParser and open config.ini
+config = ConfigParser()
+config.read('config.ini')
+
+# Load variables
+TOKEN = config['SETTINGS']['token']
+GUILD_ID = config['SETTINGS']['guild_id']
+
+COMMAND_CHAR = config['SETTINGS']['command_prefix']
+COMMANDS = [f'{COMMAND_CHAR}help', f'{COMMAND_CHAR}meetingtime']
+
+# If any of these variables aren't present, print an error and quit
 if TOKEN == None:
-    print('Error: No DISCORD_TOKEN environment variable present.')
+    print('Error: No DISCORD_TOKEN present in config.ini')
     quit()
 
 if GUILD_ID == None:
-    print('Error: No DISCORD_GUILD_ID environment variable present.')
+    print('Error: No DISCORD_GUILD_ID present in config.ini')
     quit()
 
+if COMMAND_CHAR == None:
+    print('Error: No COMMAND_PREFIX present in config.ini')
+    quit()
+    
 GUILD_ID = int(GUILD_ID)
-
-# Load variables
-COMMAND_CHAR = '!'
-COMMANDS = [f'{COMMAND_CHAR}help', f'{COMMAND_CHAR}meetingtime']
 
 # Set the intents of the bot, to be given to the `Client` constructor
 intents = discord.Intents.default()
@@ -46,7 +54,7 @@ async def on_ready():
 
 # Runs whenever a member joins
 @client.event
-async def on_member_join(member):
+async def on_member_join(member: discord.Member):
 
     # Print to terminal
     print(f'{member.name} joined the server.')
@@ -103,12 +111,13 @@ def is_command(word: str):
 
 # Take a command and its parameters and return the corresponding response
 def handle_command(command: str, params: list[str]):
-
-    match command:
-        case '!help':
+    
+    # Command is parsed to remove command prefix
+    match command[1:]:
+        case 'help':
             pretty_commands = '\n'.join(COMMANDS)
             return f'These are some of the commands I can respond to:\n`{pretty_commands}`' # Backticks (`) format text as code in discord
-        case '!meetingtime':
+        case 'meetingtime':
             return f'The current meeting time is:\nEvery Wednesday at 5:30pm'
 
 
